9693110f99fb3b08dac86d50690be015bcb78388
GanamosDiversion <mariano.ambort.2014@gmail.com>
2026-03-25 15:00:21 -0500

    100%

---
+++ app/(main)/page.js
@@ -0,0 +1,8 @@
+import LandingPage from "@/_Pages/main/contenido/inicio"
+export default function Login(){
+    return(
+        <div>
+            <LandingPage></LandingPage>
+        </div>
+    )
+}
